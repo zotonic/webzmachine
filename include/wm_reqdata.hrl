411c431865444bbebb47c2a219f9da3f45c21b0c
Marc Worrell <marc@worrell.nl>
2013-03-08 23:44:58 +0100

    Add type specs for some headers. From https://github.com/basho/webmachine/commit/ff54bd3cda8bbf65393204adc271441e0fa00230

--- include/wm_reqdata.hrl
+++ include/wm_reqdata.hrl
@@ -3,20 +3,35 @@
     socket=undefined,
     metadata=dict:new(),
     range=undefined,
-    peer=undefined,
+    peer=undefined :: inet:ip_address(),
     bodyfetch=undefined,
     log_data=undefined,
 
     % Reqdata
-    method, scheme, version,
-    disp_path, path, raw_path, path_info, path_tokens,
-    app_root,response_code,max_recv_body,
-    req_cookie, req_qs, req_headers, req_body,
-    resp_redirect, resp_headers, resp_body,
-    host_tokens, port,
+    method,
+    scheme  :: http | https,
+    version :: wrq:version(),
+    disp_path,
+    path     :: string(),
+    raw_path :: string(),
+    path_info :: dict(),
+    path_tokens :: [string()],
+    app_root  :: string(),
+    response_code :: pos_integer(),
+    max_recv_body :: pos_integer(),
+    req_cookie    :: string(),
+    req_qs        :: string(),
+    req_headers   :: gb_tree(), %% mochiheaders
+    req_body,
+    resp_redirect :: boolean(),
+    resp_headers,
+    resp_body  :: any(),
+    resp_range :: atom(),
+    host_tokens  :: [string()],
+    port  :: inet:port_number(),
 
 	%% Cache of resource calls
-	cache=[]
+	cache=[] :: list()
 }).
 
 -record(wm_controller, {
